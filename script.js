ae75a85e25528c9b326faf0f848ea6513473bb71
tamanate <tamanateaminata08@gmail.com>
2023-10-09 17:08:55 +0200

    fin modif

---
+++ script.js
@@ -0,0 +1,27 @@
+let count = 0
+let color = "black"
+
+function setIncrease(){
+    count = count+1;
+    document.getElementById("number").innerHTML = count;
+    document.getElementById("number").style.color = "green";
+
+}
+document.getElementById("increase")
+.addEventListener("click", setIncrease);
+
+function setReset(){
+    count = 0;
+    document.getElementById("number").innerHTML = count;
+    document.getElementById("number").style.color = "black";
+}
+document.getElementById("reset")
+.addEventListener("click", setReset);
+
+function setDecrease(){
+    count = count-1;
+    document.getElementById("number").innerHTML = count;
+    document.getElementById("number").style.color = "red";
+}
+document.getElementById("decrease")
+.addEventListener("click", setDecrease);
