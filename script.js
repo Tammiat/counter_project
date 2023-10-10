919e25960dd82f51988f6a33d578692f129de82e
tamanate <tamanateaminata08@gmail.com>
2023-10-10 11:27:20 +0200

    fin de modif

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 let count = 0
 let color = "black"
 
-function setIncrease(){
-    count = count+1;
+function setDecrease(){
+    count = count - 1;
+    changeColor();
     document.getElementById("number").innerHTML = count;
-    if (count > 0) {
-        color = "green";
-      }
-
 }
-document.getElementById("increase")
-.addEventListener("click", setIncrease);
+document.getElementById("decrease")
+.addEventListener("click", setDecrease);
 
 function setReset(){
     count = 0;
+    changeColor();
     document.getElementById("number").innerHTML = count;
-    document.getElementById("number").style.color = "black";
-    
 }
 document.getElementById("reset")
 .addEventListener("click", setReset);
 
-function setDecrease(){
-    count = count-1;
+function setIncrease(){
+    count = count + 1;
+    changeColor();
     document.getElementById("number").innerHTML = count;
-    document.getElementById("number").style.color = "red";
 }
-document.getElementById("decrease")
-.addEventListener("click", setDecrease);
+document.getElementById("increase")
+.addEventListener("click", setIncrease);
+
+function changeColor(){
+    if (count < 0) {
+        document.getElementById("number").style.color = "red";
+    } else if (count > 0) {
+        document.getElementById("number").style.color = "green";
+    } else if (count == 0 ){
+        document.getElementById("number").style.color = "black";
+    }
+}
